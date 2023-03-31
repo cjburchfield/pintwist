421c178a3db10e3a3bf73b3edd0dbf8f23864504
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-03-31 17:15:53 -0400

    Frontend intro complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,14 +6,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import configureStore from './store';
-// import csrfFetch from "./store/csrf";
+import csrfFetch from "./store/csrf";
 // import * as sessionActions from './store/session';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
   window.store = store;
-  // window.csrfFetch = csrfFetch;
+  window.csrfFetch = csrfFetch;
   // window.sessionActions = sessionActions;
 }
 
