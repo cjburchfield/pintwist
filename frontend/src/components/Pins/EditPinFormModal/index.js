440ecda5ce7027ce1c7db0ef6a32d8ae677cb7f7
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-04-11 22:30:54 -0400

    First weak complete

--- frontend/src/components/Pins/EditPinFormModal/index.js
+++ frontend/src/components/Pins/EditPinFormModal/index.js
@@ -1,23 +1,49 @@
+// import React, { useState } from 'react';
+// import { Modal } from '../../../context/Modal';
+// import EditPinForm from './EditPinForm';
+
+// const EditPinFormModal = ({ pin }) => {
+//   const [showModal, setShowModal] = useState(false);
+
+//   console.log("you hit the editpinformmodal index page")
+
+//   return (
+//     <>
+//       <button className="edit-pin-button" onClick={() => setShowModal(true)}>Edit Pin</button>
+//       {showModal && (
+//         <Modal onClose={() => setShowModal(false)}>
+//           <EditPinForm pinId={pin.id} onClose={() => setShowModal(false)}/>
+//         </Modal>
+//       )}
+// </>
+// )
+//       }
+
+// export default EditPinFormModal;
+
 import React, { useState } from 'react';
 import { Modal } from '../../../context/Modal';
 import EditPinForm from './EditPinForm';
 
-const EditPinFormModal = ({ pin }) => {
-  const [showModal, setShowModal] = useState(false);
+const EditPinFormModal = ({ pin, onClose }) => {
+  const [showModal, setShowModal] = useState(true);
 
-  console.log("you hit the editpinformmodal index page")
+  console.log("you hit the editpinformmodal index page");
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    onClose();
+  };
 
   return (
     <>
-      <button className="edit-pin-button" onClick={() => setShowModal(true)}>Edit Pin</button>
       {showModal && (
-        <Modal onClose={() => setShowModal(false)}>
-          <EditPinForm pinId={pin.id} onClose={() => setShowModal(false)}/>
+        <Modal onClose={handleCloseModal}>
+          <EditPinForm pinId={pin.id} onClose={handleCloseModal}/>
         </Modal>
       )}
-</>
-)
-      }
+    </>
+  );
+};
 
 export default EditPinFormModal;
-
